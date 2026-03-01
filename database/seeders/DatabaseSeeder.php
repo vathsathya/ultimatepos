49a84aacaeb80f5774469c8f7e8be5795e215071
Vath Sathya <vath.sathya@gmail.com>
2026-03-02 02:04:52 +0700

    update database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,6 @@ class DatabaseSeeder extends Seeder
             BarcodesTableSeeder::class,
             PermissionsTableSeeder::class,
             CurrenciesTableSeeder::class,
-            DummyBusinessSeeder::class,
         ]);
     }
 }
